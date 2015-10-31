6f25cc00af8a607910b7c0ac212949e7b3a2aa26
sameer thite <sameer.thite@rediffmail.com>
2015-10-31 15:36:13 +0530

    First commit

---
+++ js/galleries.js
@@ -0,0 +1,439 @@
+// GALLERY 1
+jQuery('#gallery1').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-s.jpg',
+            'subHtml': ''
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-s.jpg',
+            'subHtml': ""
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-s.jpg',
+            'subHtml': ""
+        }, {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 2
+jQuery('#gallery2').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-s.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-s.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-s.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-s.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 3
+jQuery('#gallery3').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-thumb.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-thumb.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-thumb.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-thumb.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 4
+jQuery('#gallery4').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-thumb.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-thumb.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-thumb.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-thumb.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 5
+jQuery('#gallery5').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-thumb.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-thumb.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-thumb.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-thumb.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 6
+jQuery('#gallery6').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-thumb.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-thumb.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-thumb.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-thumb.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 7
+jQuery('#gallery7').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-thumb.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-thumb.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-thumb.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-thumb.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
+
+// GALLERY 8
+jQuery('#gallery8').on('click', function (e) {
+    "use strict";
+    e.preventDefault();
+    jQuery(this).lightGallery({
+        // Settings
+        dynamic: true,
+        mode: 'lg-slide-vertical',
+        zoom: true,
+        fullScreen: true,
+        autoplay: false,
+        thumbnail: true,
+        download: true,
+        counter: true,
+        // Images
+        dynamicEl: [{
+            'src': 'images/photos/p8.jpg',
+            'thumb': 'images/photos/p8-thumb.jpg',
+            'subHtml': 'IKCOC'
+        }, {
+            'src': 'images/photos/p1.jpg',
+            'thumb': 'images/photos/p1-thumb.jpg',
+            'subHtml': "GSA Academy"
+        }, {
+            'src': 'images/photos/p2.jpg',
+            'thumb': 'images/photos/p2-thumb.jpg',
+            'subHtml': "Quae expetendis"
+        }, {
+            'src': 'images/photos/p3.jpg',
+            'thumb': 'images/photos/p3-thumb.jpg',
+            'subHtml': "Non nulla"
+        },
+        {
+            'src': 'images/photos/p5.jpg',
+            'thumb': 'images/photos/p5-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p6.jpg',
+            'thumb': 'images/photos/p6-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p7.jpg',
+            'thumb': 'images/photos/p7-s.jpg',
+            'subHtml': ""
+        },
+            {
+            'src': 'images/photos/p4.jpg',
+            'thumb': 'images/photos/p4-s.jpg',
+            'subHtml': ""
+        }]
+    })
+});
